646d527db21be5ab335845abcb0b7eba3760c101
jayslen <jayslen134@gmail.com>
2025-09-18 07:27:31 -0400

    feat: add CORS support and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import CookieParser from 'cookie-parser'
+import cors from 'cors'
 import { createAuthRoutes } from './routes/authRoutes.js';
 import { createBoardRouter } from './routes/boardRoutes.js';
 import { MySqlModel } from './models/mysqlModel.js'
@@ -10,11 +11,23 @@ const app = express();
 const authRoutes = await createAuthRoutes(MySqlModel)
 const boardRoutes = await createBoardRouter(MySqlModel)
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+})
+
 app.use(json())
 app.use(CookieParser())
 
 app.use('/board', boardRoutes)
-app.use(authRoutes)
+app.use('/', authRoutes)
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running at http://localhost:${SERVER_PORT}`);
